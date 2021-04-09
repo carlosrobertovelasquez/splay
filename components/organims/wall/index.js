bfcf9701b0e32068de9a75ba213cf7d30d73d461
Carlos Velasquez <carlosrobertovelasquez@gmail.com>
2021-04-09 00:53:47 -0600

    Solo demo

--- components/organims/wall/index.js
+++ components/organims/wall/index.js
@@ -5,7 +5,7 @@ export default function index() {
     console.log('user wall',user)
     return (
         <div>
-            <h1>holllaaa</h1>
+            <h1>Aqui va el muro esta en codificacion en este momento</h1>
              <div className="md:float-right sm:float-none py-6 ">
                   <button onClick={()=>signout()} type="submit" className=" rounded-xl mt-5 p-3 text-white text-sm bg-blue-medium hover:bg-blue-medium focus:outline-none focus:bg-blue-700 duration-300">Salir</button>
               </div>
